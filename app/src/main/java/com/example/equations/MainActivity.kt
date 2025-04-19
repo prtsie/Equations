52f8d7533a2433d70a59b8a058bbd7ef9c5b5e5f
prtsie <yonhsup@mail.ru>
2025-04-19 11:24:02 +0300

    changing background color

--- app/src/main/java/com/example/equations/MainActivity.kt
+++ app/src/main/java/com/example/equations/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,15 +20,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 import kotlin.random.Random
 
@@ -40,6 +46,9 @@ var operation: MutableState<Op>? = null
 var answer = 0
 var input: MutableState<String>? = null
 var parsedInput: MutableState<Int>? = null
+var backColor: MutableState<Color>? = null
+var lastAnswerIsCorrect = false
+var colorSaturation = 0.0f
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,41 +68,56 @@ fun Greeting() {
     correct = rememberSaveable { mutableIntStateOf(0) }
     incorrect = rememberSaveable { mutableIntStateOf(0) }
     parsedInput = rememberSaveable { mutableIntStateOf(0) }
-    input = rememberSaveable { mutableStateOf("0") }
+    input = rememberSaveable { mutableStateOf("") }
     operation = rememberSaveable { mutableStateOf(Op.Add) }
+    backColor = remember { mutableStateOf(Color.White) }
+    val coroutineScope = rememberCoroutineScope()
 
     fun generateEquation() {
-        first!!.value = Random.nextInt(100)
+        first!!.value = Random.nextInt(10, 89)
         operation!!.value = Op.entries[Random.nextInt(Op.entries.count())]
         when (operation!!.value) {
             Op.Add -> {
-                second!!.value = Random.nextInt(99 - first!!.value)
+                second!!.value = Random.nextInt(10, 99 - first!!.value)
                 answer = first!!.value + second!!.value
             }
 
             Op.Subtract -> {
-                second!!.value = Random.nextInt(first!!.value + 1)
+                second!!.value = Random.nextInt(10, first!!.value + 1)
                 answer = first!!.value - second!!.value
             }
 
             Op.Divide -> {
-                var value = 1
-                while (first!!.value % value != 0) {
-                    value = Random.nextInt(first!!.value + 1)
-                }
-                second!!.value = value
+                second!!.value = Random.nextInt(1, 49)
+                first!!.value = second!!.value * Random.nextInt(1, 99 / second!!.value)
                 answer = first!!.value / second!!.value
             }
 
             Op.Multiple -> {
-                second!!.value = Random.nextInt(99 / first!!.value)
+                second!!.value = Random.nextInt( 99 / first!!.value)
                 answer = first!!.value * second!!.value
             }
         }
     }
 
+    fun coloredOutput() {
+        coroutineScope.launch {
+            while (colorSaturation < 1.0f) {
+                if (lastAnswerIsCorrect) {
+                    backColor!!.value = Color.White.copy(red = colorSaturation, blue = colorSaturation)
+                } else {
+                    backColor!!.value = Color.White.copy(green = colorSaturation, blue = colorSaturation)
+                }
+                colorSaturation += 0.01f
+                delay(10)
+            }
+        }
+    }
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backColor!!.value),
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -156,9 +180,13 @@ fun Greeting() {
                 {
                     if (parsedInput!!.value == answer) {
                         correct!!.value++
+                        lastAnswerIsCorrect = true
                     } else {
                         incorrect!!.value++
+                        lastAnswerIsCorrect = false
                     }
+                    colorSaturation = 0.7f
+                    coloredOutput()
                     input!!.value = ""
                     generateEquation()
                 },
